fdc96366f82ce5a94bb771a7816f2dd59313596e
elpistelero <73737679+elpistelero@users.noreply.github.com>
2022-03-31 20:04:37 +0530

    Create Plot3.R

---
+++ Plot3.R
@@ -0,0 +1,16 @@
+# ggplot2
+library(ggplot2)
+
+#  data
+neiData <- readRDS("./exp-data/summarySCC_PM25.rds")
+sccData <- readRDS("./exp-data/Source_Classification_Code.rds")
+
+# Baltimore City, Maryland == fips
+subset <- neiData[neiData$fips == "24510", ] 
+par("mar"=c(5.1, 4.5, 4.1, 2.1))
+
+# Result
+png(filename = "./exp-data/xxx-2/plot3.png", width = 480, height = 480, units = "px")
+g <- ggplot(subset, aes(year, Emissions, color = type))
+g + geom_line(stat = "summary", fun.y = "sum") + ylab(expression('Total PM'[2.5]*" Emissions")) + ggtitle("Total Emissions in Baltimore City from 1999 to 2008")
+dev.off()
